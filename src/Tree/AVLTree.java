0237b7c64ee8db9d5fb5b8a263b18eec80fd586a
RahulChaudhari <117882038+Ral53@users.noreply.github.com>
2024-06-06 07:57:45 +0545

    before Heap

--- src/Tree/AVLTree.java
+++ src/Tree/AVLTree.java
@@ -14,12 +14,48 @@ public class AVLTree {
         } else {
             return root;
         }
+
         root.height = 1 + Math.max(getHeight(root.left), getHeight(root.right));
         int balanceFactor = getBalanceFactor(root);
 
-        // LL, LR, RR, RL
+        if (balanceFactor > 1 && data < root.left.data) {   // LL
+            leftRotationTree(root);
+        }
 
+        if (balanceFactor > 1 && data > root.left.data) {   // LR
+            root.left = leftRotationTree(root.left);
+            return rightRotateTree(root);
+        }
+
+        if (balanceFactor > -1 && data > root.right.data) {     // RR
+            rightRotateTree(root);
+        }
+
+        if (balanceFactor > -1 && data < root.right.data) {     // RL
+            root.right = rightRotateTree(root.right);
+            leftRotationTree(root);
+        }
         return root;
+    }
+
+    TreeNode rightRotateTree(TreeNode y) {
+        TreeNode x = y.left;  // y = root
+        TreeNode t2 = x.right;
+        x.right = y;
+        y.left = t2;
+        y.height = 1+Math.max(getHeight(y.left), getHeight(y.right));
+        x.height = 1+Math.max(getHeight(x.left), getHeight(x.right));
+        return x;
+    }
+
+    TreeNode leftRotationTree(TreeNode x) {
+        TreeNode y = x.right;  // x = root
+        TreeNode t2 = y.right;
+        y.left = x;
+        x.right = t2;
+        x.height = 1+Math.max(getHeight(x.left), getHeight(x.right));
+        y.height = 1+Math.max(getHeight(y.left), getHeight(y.right));
+        return y;
     }
 
     int  getHeight(TreeNode node) {
